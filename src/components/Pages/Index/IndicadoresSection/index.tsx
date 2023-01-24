0e47fad436e66d697169a0bec48d94bfba214ee9
Yasmin <yasminsdcastro@gmail.com>
2023-01-24 09:45:47 -0300

    feat: images

--- src/components/Pages/Index/IndicadoresSection/index.tsx
+++ src/components/Pages/Index/IndicadoresSection/index.tsx
@@ -20,12 +20,24 @@ const IndicadoresSection: React.FC = () => {
             </div>
           </Title>
           <Content>
-            <Indicador title="Geográfico (profissionais das artes)" />
+            <Indicador
+              title="Geográfico (profissionais das artes)"
+              image="indicadores/artistas-geografico.png"
+            />
             <Indicador title="Categoria informada no Instagram (Ex: artista, arte etc.)" />
-            <Indicador title="Quantidade de seguidores (profissionais das artes)" />
+            <Indicador
+              title="Quantidade de seguidores (profissionais das artes)"
+              image="indicadores/artistas-quantidade-seguidores.png"
+            />
             <Indicador title="Perfil verificado (profissionais das artes)" />
-            <Indicador title="Geográfico (instituições de arte)" />
-            <Indicador title="Quantidade de seguidores (instituições de arte)" />
+            <Indicador
+              title="Geográfico (instituições de arte)"
+              image="indicadores/instituicoes-geografico.png"
+            />
+            <Indicador
+              title="Quantidade de seguidores (instituições de arte)"
+              image="indicadores/instituicoes-quantidade-seguidores.png"
+            />
           </Content>
         </ContentBlock>
       </Container>
