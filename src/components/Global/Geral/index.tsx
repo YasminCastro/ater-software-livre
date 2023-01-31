4482292891b59426f5919188d8c3f224366d24de
Yasmin <yasminsdcastro@gmail.com>
2023-01-31 15:13:35 -0300

    rtefactor: title

--- src/components/Global/Geral/index.tsx
+++ src/components/Global/Geral/index.tsx
@@ -3,9 +3,7 @@ import { TitleWrapper } from "./styles";
 export const Title: React.FC<{ title: string }> = ({ title }) => {
   return (
     <TitleWrapper>
-      <div>
-        <h2>{title}</h2>
-      </div>
+      <h2>{title}</h2>
     </TitleWrapper>
   );
 };
--- src/components/Global/Geral/styles.ts
+++ src/components/Global/Geral/styles.ts
@@ -10,14 +10,20 @@ export const TitleWrapper = styled.div`
     font-size: 40px;
   }
 
+  @media (max-width: 1300px) {
+    margin-bottom: 20px;
+  }
+
   @media (max-width: 1100px) {
     h2 {
       font-size: 30px;
     }
   }
 
-  @media (max-width: 1300px) {
-    margin-bottom: 20px;
+  @media (max-width: 700px) {
+    h2 {
+      font-size: 24px;
+    }
   }
 `;
 
@@ -29,4 +35,14 @@ export const Square = styled.div`
   border: 1px solid ${colors.gray};
   left: 0px;
   margin: 50px auto;
+
+  @media (max-width: 1100px) {
+    width: 42px;
+    height: 37px;
+  }
+
+  @media (max-width: 700px) {
+    width: 32px;
+    height: 27px;
+  }
 `;
