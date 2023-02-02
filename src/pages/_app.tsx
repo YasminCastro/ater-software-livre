457b3b96bdd08e0e2726d179c8eaa5a5c307ea49
Yasmin <yasminsdcastro@gmail.com>
2023-02-02 15:40:36 -0300

    bugfix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
-import ResponsiveProvider from "@/Providers/MobileHeader";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ResponsiveProvider>
-      <MantineProvider theme={{ fontFamily: "'Lato', sans-serif" }}>
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ResponsiveProvider>
+    <MantineProvider theme={{ fontFamily: "'Lato', sans-serif" }}>
+      <Component {...pageProps} />
+    </MantineProvider>
   );
 }
