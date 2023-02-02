5c138fbe053276b44c818260bfe6aef872a35cf3
Yasmin <yasminsdcastro@gmail.com>
2023-02-02 15:15:55 -0300

    feat: sep

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
+import SEO from "@/components/Global/SEO";
 import IndicadoresSection from "@/components/Pages/Index/IndicadoresSection";
 import MetodologiaSection from "@/components/Pages/Index/MetodologiaSection";
 import ParticipeSection from "@/components/Pages/Index/ParticipeSection";
 import SoftwareLivreSection from "@/components/Pages/Index/SoftwareLivreSection";
 import { PageContainer, PageWrapper } from "@/styles/Page";
-import styled from "styled-components";
 import Layout from "../components/Global/Layout";
 
 export default function Home() {
   return (
     <>
+      <SEO title="Software" />
       <Layout>
         <PageWrapper>
           <PageContainer>
