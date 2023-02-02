bc93a5850ff13ad597995a24e0888c607c801725
Yasmin <yasminsdcastro@gmail.com>
2023-02-02 12:30:43 -0300

    css footer fixed

--- src/components/Global/Footer/styles.ts
+++ src/components/Global/Footer/styles.ts
@@ -1,27 +1,21 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
+  background: #4c96cb;
+
   width: 100%;
   max-width: 100vw;
 
-  background: #4c96cb;
-  transition: background 0.15s linear;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const FooterContainer = styled.div`
   width: 100%;
-  max-width: 1024px;
+  max-width: 1020px;
 
-  padding: 20px 0;
-  transition: 0.2s;
-
-  @media (max-width: 1088px) {
-    margin: 0 32px;
-  }
-
-  @media (max-width: 500px) {
-    margin: 0 20px;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
