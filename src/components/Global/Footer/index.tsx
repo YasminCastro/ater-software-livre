aa47054a71b8ce2101f95b9824e265bf49d6e71c
Yasmin <yasminsdcastro@gmail.com>
2023-02-09 11:37:29 -0300

    feat: img alt

--- src/components/Global/Footer/index.tsx
+++ src/components/Global/Footer/index.tsx
@@ -4,7 +4,10 @@ const Footer: React.FC = () => {
   return (
     <FooterWrapper>
       <FooterContainer>
-        <img src="footer.svg" />
+        <img
+          src="footer.svg"
+          alt="Logo da PROAC, do projeto Ater e do Governo do Estado de São Paulo"
+        />
       </FooterContainer>
     </FooterWrapper>
   );
--- src/components/Pages/Index/IndicadoresSection/index.tsx
+++ src/components/Pages/Index/IndicadoresSection/index.tsx
@@ -23,27 +23,45 @@ const ParticipeSection: React.FC = () => {
         <InticadorContainer>
           <IndicadorItem>
             <p>Geográfico (profissionais das artes)</p>
-            <img src="indicadores/artistas-geografico.png" />
+            <img
+              src="indicadores/artistas-geografico.png"
+              alt="Gráfico com o mapa do Brasil, informando o número de profissionais das artes por estado."
+            />
           </IndicadorItem>
           <IndicadorItem>
             <p>Categoria informada no Instagram (Ex: artista, arte etc.)</p>
-            <img src="indicadores/nuvem_1.png" />
+            <img
+              src="indicadores/nuvem_1.png"
+              alt="Diversas palavras relacionadas a arte."
+            />
           </IndicadorItem>
           <IndicadorItem>
             <p>Quantidade de seguidores (profissionais das artes)</p>
-            <img src="indicadores/artistas-quantidade-seguidores.png" />
+            <img
+              src="indicadores/artistas-quantidade-seguidores.png"
+              alt="Gráfico mostrando o número de seguidores para profissionais das artes."
+            />
           </IndicadorItem>
           <IndicadorItem>
             <p>Perfil verificado (profissionais das artes)</p>
-            <img src="indicadores/nuvem_2.png" />
+            <img
+              src="indicadores/nuvem_2.png"
+              alt="Diversas palavras relacionadas a artistas."
+            />
           </IndicadorItem>
           <IndicadorItem>
             <p>Geográfico (instituições de arte)</p>
-            <img src="indicadores/instituicoes-geografico.png" />
+            <img
+              src="indicadores/instituicoes-geografico.png"
+              alt="Gráfico com o mapa do Brasil, informando o número de instituições de arte por estado."
+            />
           </IndicadorItem>
           <IndicadorItem>
             <p>Quantidade de seguidores (instituições de arte)</p>
-            <img src="indicadores/instituicoes-quantidade-seguidores.png" />
+            <img
+              src="indicadores/instituicoes-quantidade-seguidores.png"
+              alt="Gráfico mostrando o número de seguidores para instituições de arte."
+            />
           </IndicadorItem>
         </InticadorContainer>
       </Container>
